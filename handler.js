f56218548632905142645faaa2369b29219fca4b
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2019-04-15 14:19:20 +0200

    Initial commit

---
+++ handler.js
@@ -0,0 +1,50 @@
+// @format
+'use strict';
+
+const AWS = require('aws-sdk');
+
+const bucket = 'cinemarket-videos';
+const signedUrlExpireSeconds = 60 * 5;
+
+function response(statusCode, data) {
+  return {
+    statusCode,
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+}
+
+module.exports.sign = async (event, context, callback) => {
+  AWS.config.update({
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
+  });
+  const s3 = new AWS.S3();
+
+  let body;
+  if (event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    callback(response(400, {error: 'No json body present'}));
+  }
+
+  // NOTE: s3.getSignedUrl doesn't support promises:
+  // https://github.com/aws/aws-sdk-js/issues/1008
+  s3.getSignedUrl(
+    'putObject',
+    {
+      Bucket: bucket,
+      Key: body.filename,
+      Expires: signedUrlExpireSeconds,
+    },
+    (error, url) => {
+      if (error) {
+        callback(response(500, {error: error.message}));
+      } else {
+        callback(response(200, {url}));
+      }
+    },
+  );
+};
